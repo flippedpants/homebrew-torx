dd9538fd77a4909b414ccf94d7a347ad7c30d4cf
flippedpants <dakshrandomstuff@gmail.com>
2026-07-21 20:00:29 +0530

    changed the version to v0.3.2

--- Formula/torx.rb
+++ Formula/torx.rb
@@ -1,26 +1,26 @@
 class Torx < Formula
   desc "BitTorrent client made in rust with concurrent multi-peer downloading"
   homepage "https://github.com/flippedpants/torx"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/flippedpants/torx/releases/download/v#{version}/torx-aarch64-apple-darwin.tar.gz"
-      sha256 "c614bc427712845096aa149069556b9b34ebbbe028ba6b91a31f8a56add509ae"
+      sha256 "478e47ca7190c7f05d38f23122491cfb7a48d1c6ebdfed20949ab9edbb83eba9"
     else
       url "https://github.com/flippedpants/torx/releases/download/v#{version}/torx-x86_64-apple-darwin.tar.gz"
-      sha256 "4f98970e046e7aad37193782b31424196ce6b219d300b06babb450fb4c9d650a"
+      sha256 "901dc6a9e54c08b908d1a47f900df30fe868ea8cba3a4e91d7d6e9410a3b9944"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
         url "https://github.com/flippedpants/torx/releases/download/v#{version}/torx-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "6e657244b3af622e18905d87a08b7b0839a426813a652aebffddca9970813eb4"
+        sha256 "1bf7969bf701d76109eb2d51a740bffe6cd30eb41d3bb1fd096aa6a08295b0d6"
     else
         url "https://github.com/flippedpants/torx/releases/download/v#{version}/torx-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "ad8721eeb8cdc10ef7385ef496a2ec60f05667a49a0a3e0b78b01f5375193e88"
+        sha256 "c5a5a5f95407d4f7cffc3d80effd55dfbc0eee6986b1693b71fa0c314a7de859"
     end
   end
 
